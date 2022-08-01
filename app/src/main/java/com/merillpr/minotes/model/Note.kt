2d7eda0665564bb0b18114285751d628d65e030c
Meril Lia Priday Riskiana <merillpr16@gmail.com>
2022-08-01 08:58:27 +0700

    1. Mengubah data class Note

--- app/src/main/java/com/merillpr/minotes/model/Note.kt
+++ app/src/main/java/com/merillpr/minotes/model/Note.kt
@@ -1,11 +1,14 @@
 package com.merillpr.minotes.model
 
-
 data class Note(
     val id: Int,
     val activity: String,
-    val start: String,
-    val end: String,
-    val date: String,
+    val startHour: Int,
+    val startMinute: Int,
+    val endHour: Int,
+    val endMinute: Int,
+    val year: Int,
+    val month: Int,
+    val day: Int,
     val repeat: String
 )
